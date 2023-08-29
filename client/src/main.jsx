31cb0bb0c8a748c7cfd971e3f629a72457d9f47d
Jonavoe <voeffray.jonathan@gmail.com>
2023-08-29 14:39:59 -0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,7 +14,7 @@ const getServerUrl = () => {
   if (hostname.includes("localhost")) {
     return "http://localhost:3001/api";
   } else if (hostname.includes("ventas-jonavoe.vercel")) {
-    return "https://empleadosjonavoe.onrender.com";
+    return "https://empleadosjonavoe.onrender.com/api";
   }
   // else if (hostname.includes("sml-app.com")) {
   //   return "https://sml-app.com/api";
